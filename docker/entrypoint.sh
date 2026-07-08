52a3304e7e375a864b184cbe7f42f83716cd0c14
MamoudaZouley <zoulahatoumamouda@gmail.com>
2026-07-08 15:13:49 +0100

    Fix PostgreSQL configuration for Render

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
+
+php artisan config:clear
+php artisan cache:clear
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
 php artisan migrate --force
+
 exec /usr/bin/supervisord -c /etc/supervisord.conf
